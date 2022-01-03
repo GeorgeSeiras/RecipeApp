3473ed258807f78e5ff1615792e6f8442a0afabc
GeorgeSeiras <gsiras1999@gmail.com>
2022-01-03 21:13:22 +0200

    fix: nav missalignment/ swapped dropdown for navdropdown

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -2,22 +2,24 @@ import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../Context/authContext';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Dropdown from 'react-bootstrap/Dropdown';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import NAV_LOGO from '../../static/nav_logo.png';
+import NO_AVATAR from '../../static/no_avatar.svg';
+
 import './NavBar.css'
 const Navigation = () => {
     const userData = useContext(UserContext);
-    const [avatar,setAvatar] = useState(null);
+    const [avatar, setAvatar] = useState(null);
 
-    useEffect(()=>{
-        setAvatar(userData?.user?.user?.avatar||NAV_LOGO)
-    },[])
-    
+    useEffect(() => {
+        setAvatar(userData?.user?.user?.avatar || NO_AVATAR)
+    }, [userData])
+
     return (
         <>
             <Navbar sticky='top' collapseOnSelect fixed='top' expand='sm' bg='secondary' variant='dark'>
                 <Navbar.Brand id="Brand" href="/">
-                    <img src={NAV_LOGO} width="70px" height="auto" className="img-responsive" />
+                    <img src={NAV_LOGO} width="50px" height="auto" alt="logo" className="img-responsive" />
                     RecipeApp
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls='responsive-navbar-nav' />
@@ -30,20 +32,15 @@ const Navigation = () => {
                                 <Nav.Link href='/register'>Register</Nav.Link>
                             </Nav>
                         }
-                        {(userData.user.token && userData.user.user) &&
-                            <Nav className="ms-auto">
-                                <Dropdown id="dropdown-basic-button" drop="start">
-                                    <Dropdown.Toggle  variant="info" id="dropdown-basic">
-                                        
-                                        <img  src={avatar} width="20px" height="auto"></img>
-                                    </Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item href={'/user/' + userData.user.user.username}>Profile</Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            </Nav>
-                        }
                     </Nav>
+                    {(userData.user.token && userData.user.user) &&
+                        <Nav className="ms-auto">
+                            <NavDropdown id="dropdown-basic-button" drop="start"
+                                title={<img className="avatar" src={avatar} alt='user pic' width="40px" height="auto" />}>
+                                <NavDropdown.Item href={'/user/' + userData.user.user.username}>Profile</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    }
                 </Navbar.Collapse>
             </Navbar>
         </>
