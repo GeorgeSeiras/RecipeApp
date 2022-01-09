231ab1b2f1f3bd0fdf4a141a4670c0fa5d8f2aec
GeorgeSeiras <gsiras1999@gmail.com>
2022-01-09 16:54:34 +0200

    fix:recipe creation now works with new image configuration

--- backend/recipe/serializers.py
+++ backend/recipe/serializers.py
@@ -9,19 +9,20 @@ class IngredientSerializer(serializers.ModelSerializer):
     amount = serializers.FloatField()
     unit = serializers.CharField(required=False)
     ingredient = serializers.CharField()
-    recipe = serializers.RelatedField(source='recipe.recipe', read_only=True)
+    recipe = serializers.RelatedField(source="recipe.recipe", read_only=True)
 
     class Meta:
         model = Ingredient
-        fields = '__all__'
+        fields = "__all__"
 
-    def create(self, validated_data, recipe):
+    def create( validated_data, recipe):
         return Ingredient.objects.create(**validated_data, recipe=recipe)
 
 
 class RecipeCreateSerializer(serializers.ModelSerializer):
     photo = serializers.ImageField(
-        required=False, allow_empty_file=True, max_length=None)
+        required=False, allow_empty_file=True, max_length=None
+    )
     title = serializers.CharField()
     prep_time = serializers.IntegerField()
     cook_time = serializers.IntegerField()
@@ -32,13 +33,11 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = '__all__'
+        fields = "__all__"
 
 
 class RecipeSerializer(serializers.Serializer):
-    user = serializers.RelatedField(source='user.user', read_only=True)
-    images = serializers.ListField(
-        child=RecipeImageSerializer(), required=False)
+    user = serializers.PrimaryKeyRelatedField(many=False, queryset=User.objects.all())
     title = serializers.CharField()
     prep_time = serializers.IntegerField()
     cook_time = serializers.IntegerField()
@@ -49,38 +48,28 @@ class RecipeSerializer(serializers.Serializer):
     course = serializers.ListField(child=serializers.CharField())
     steps = serializers.ListField(child=serializers.CharField())
 
-    def create(self, validated_data):
-        ingredient_data = validated_data.pop('ingredients')
-        if('images' in validated_data):
-            images_data = validated_data.pop('images')
-            image_serializer = RecipeImageSerializer(data=images_data)
-            image_serializer.is_valid(raise_exception=True)
-            image_serializer.save()
-        recipe = Recipe.objects.create(**validated_data)
+    def create(self):
+        ingredient_data = self.validated_data.pop("ingredients")
+        recipe = Recipe.objects.create( **self.validated_data)
         ingredients = []
         for ingredient in ingredient_data:
-            created_ingredient = IngredientSerializer.create(
-                self, ingredient, recipe)
+            created_ingredient = IngredientSerializer.create(ingredient, recipe)
             ingredients.append(created_ingredient)
         res = Recipe.objects.get(pk=recipe.id)
         return res.to_dict()
 
 
 class RecipePatchSerializer(serializers.Serializer):
-    photo = serializers.ImageField(
-        required=True)
+    photo = serializers.ImageField(required=True)
     title = serializers.CharField(required=False)
     prep_time = serializers.IntegerField(required=False)
     cook_time = serializers.IntegerField(required=False)
     desc = serializers.CharField(required=False)
     ingredients = IngredientSerializer(required=False, many=True)
     servings = serializers.IntegerField(required=False)
-    cuisine = serializers.ListField(
-        required=False, child=serializers.CharField())
-    course = serializers.ListField(
-        required=False, child=serializers.CharField())
-    steps = serializers.ListField(
-        required=False, child=serializers.CharField())
+    cuisine = serializers.ListField(required=False, child=serializers.CharField())
+    course = serializers.ListField(required=False, child=serializers.CharField())
+    steps = serializers.ListField(required=False, child=serializers.CharField())
 
 
 class IngredientCreateSerializer(serializers.Serializer):
