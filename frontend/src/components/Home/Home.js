e3c852cc388fcfb7087e801af2544a2a48a9e6be
GeorgeSeiras <gsiras1999@gmail.com>
2022-01-12 22:30:46 +0200

    end of day commit

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,17 +1,75 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
+import { RecipesReducer } from './reducer'
+import { getRecipes } from "./actions"
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
+import IMAGE_NOT_FOUND from "../../static/image_not_found.svg"
 
 export default function Home(props) {
+
+    const [state, dispatch] = useReducer(RecipesReducer);
+    const [queryParams, setQueryParams] = useState([]);
+    const [response, setResponse] = useState()
+    const [thumbnails, setThumbnails] = useState([])
+    const mediaPath = 'http://localhost:8000/media/'
+
+    useEffect(() => {
+        (async () => {
+            const response = await getRecipes(dispatch, queryParams);
+            if (response) {
+                setResponse(response);
+            }
+        })()
+    }, [])
+
+    useEffect(() => {
+        setThumbnails([])
+        response?.results.forEach((recipe, key) => {
+            const img = recipe.images.find((image) => {
+                return image.type === "THUMBNAIL"
+            })?.image
+            if (img) {
+                setThumbnails(thumbnails => [...thumbnails, `${mediaPath}${img}`]);
+            } else {
+                setThumbnails(thumbnails => [...thumbnails, IMAGE_NOT_FOUND]);
+            }
+        })
+    }, [response?.results])
+    
+    const handleSubmit = async (event) => {
+        // payload.forEach((key, value, index) => {
+        //     if (index === 0) {
+        //         queryParams = queryParams.concat(`?${key}=${value}`)
+        //     } else {
+        //         queryParams = queryParams.concat(`&${key}=${value}`)
+        //     }
+        // })
+    }
+
     return (
         <div>
-            <h3>Home Page</h3>
-            <h3>Home Page</h3>
-            <h3>Home Page</h3>
-            <h3>Home Page</h3>
-            <h3>Home Page</h3>
-            <h3>Home Page</h3>
-            <h3>Home Page</h3>
-            <h3>Home Page</h3>
+            <Container>
+                {response?.results &&
+                    <Row>
+                        {response.results.map((recipe, index) => {
+                            return (
+                                <Col>
+                                    <Card>
+                                        <Card.Img variant="top"
+                                            src={`${thumbnails[index]}`}
+                                            alt='card image' />
+                                    </Card>
+                                </Col>
+                            )
+                        })}
 
+                    </Row>
+                }
+            </Container>
         </div>
     )
 }
---
+++ frontend/src/components/Home/actions.js
@@ -0,0 +1,28 @@
+const ROOT_URL = 'http://localhost:8000/api';
+
+export async function getRecipes(dispatch, payload, queryParams) {
+
+    payload = {}
+    const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    };
+
+    try {
+        dispatch({ type: 'GET_RECIPES_REQUEST' });
+        let response = null
+        if (payload) {
+            response = await fetch(`${ROOT_URL}/recipes`, requestOptions);
+        } else {
+            response = await fetch(`${ROOT_URL}/recipes${queryParams}`, requestOptions);
+        }
+        let data = await response.json();
+        if (data) {
+            dispatch({ type: 'GET_RECIPES_SUCCESS', payload: data })
+            return data
+        }
+        dispatch({ type: 'GET_RECIPES_ERROR', errorMessage: data })
+    } catch (error) {
+        dispatch({ type: 'GET_RECIPES_ERROR', errorMessage: error })
+    }
+}
