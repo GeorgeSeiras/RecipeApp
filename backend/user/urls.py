e270d5c976f992261ac815d98bbe72668190dfdd
GeorgeSeiras <gsiras1999@gmail.com>
2022-04-04 15:44:55 +0300

    add: user by id tests

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('user/password',views.ChangePassword.as_view(),name='user-password'),
     path('user/register', views.UserRegister.as_view(),name='user-register'),
     path('user/recipe', views.UserRecipes.as_view()),
-    path('user/<int:pk>', views.UserDetail.as_view()),
+    path('user/<int:pk>', views.UserDetail.as_view(),name='user-by-id'),
     path('user/<str:username>', views.UserByUsername.as_view()),
     path('user/<int:user_id>/list', views.UserLists.as_view()),
 ]
