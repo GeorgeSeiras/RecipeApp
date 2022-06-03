494c64ba3927d6bc7ae77bdf339a474745158579
GeorgeSeiras <gsiras1999@gmail.com>
2022-06-03 13:13:43 +0300

    tmp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,13 +135,13 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': env('DATABASE_NAME'),
+        'NAME': env('POSTGRES_DB'),
 
-        'USER': env('DATABASE_USER'),
+        'USER': env('POSTGRES_USER'),
 
-        'PASSWORD': env('DATABASE_PASS'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
 
-        'HOST': 'localhost',
+        'HOST': 'database',
 
         'PORT': '5432',
 
---
+++ db/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE recipeapp;
+CREATE USER recipeadmin WITH PASSWORD password;
+GRANT ALL PRIVILEGES ON DATABASE recipeapp TO recipeadmin;
